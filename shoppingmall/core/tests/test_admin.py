80defe149058e156a1c5db9656975fd8ab454d44
Jacob <jacob.juhyung.lee@gmail.com>
2021-02-20 19:03:52 +0900

    test: admin 테스트 추가

---
+++ shoppingmall/core/tests/test_admin.py
@@ -0,0 +1,40 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@admin.com',
+            password='adminpw123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@test.com',
+            password='test123',
+        )
+
+    def test_users_listed(self):
+        """유저 페이지에 유저가 리스트 됬는지 테스트"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """유저 에딧 페이지 작동 테스트"""
+        url = reverse('admin:core_user_change', args[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """유저 생성 페이지 작동 테스트"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
