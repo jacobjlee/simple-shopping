cf36d26a7d1edb430671397dffd1372140c6e8ae
Jacob <jacob.juhyung.lee@gmail.com>
2021-02-20 16:03:29 +0900

    feat: drf-yasg 사용 Swagger 추가

--- shoppingmall/config/settings/base.py
+++ shoppingmall/config/settings/base.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'drf_yasg',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
--- shoppingmall/config/urls.py
+++ shoppingmall/config/urls.py
@@ -16,8 +16,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import url
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Shoppingmall API",
+        default_version="v1",
+        description="쇼핑몰 구축을 위한 API 문서입니다.",
+        terms_of_service="https://google.com/policies/terms/",
+        contact=openapi.Contact(email="test@test.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
 ]
 
